41309738228e78e0c68e2a7fcfd8bcd4a0450c7b
Lisa <lisa.hl.chen@gmail.com>
2022-07-17 00:10:47 -0700

    added script tag at end of html doc

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
-let likes = [9,12,9]
-let comment_likes = [
+console.log("page loading...");
+
+// index     0  1   2
+var likes = [9, 12, 9];
+var comment_likes = [
     document.querySelector("#comment-1"),
     document.querySelector("#comment-2"),
-    document.querySelector("#comment-3"),
-]
+    document.querySelector("#comment-3")
+];
 
-var spanText=document.querySelector('comment-1')
-function increase(){
-    spanText.innerText+=1
+function increase(id) {
+    console.log(comment_likes[id])
+    likes[id]++;
+    comment_likes[id].innerHTML = likes[id] + " like(s)";
 }
-
-
-    // likes[index]++;
-    // comment_likes[index].innerHTML = likes[index]+"like(s)"
-
-    // new_count = likes[index]+=1;
-    // string=new_count + " likes"
-    // id="#comment-"+(index+1)
-    // console.log(id)
-    // id.innerHTML = string;
-// function like(id) {
-//     likes[id]++;
-//     spans[id].innerHTML = likes[id] + "like(s)";
-// }
-function login(element){
-    element.innerText = "Logout"
-}
-
-
-
